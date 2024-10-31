abe7a745a4fa5d066731b72f87a4ceb2b3860acb
Tales Paiva <124717378+talespcalvi@users.noreply.github.com>
2024-10-31 13:56:24 -0300

    Update teste_pilhampu.c

--- teste_pilhampu.c
+++ teste_pilhampu.c
@@ -2,14 +2,17 @@
 #include "pilhampuTAD.h"
 
 int main() {
+    // Cria a pilha para o sensor MPU
     pilhaMPU* pilha = criar_pilha();
 
+    // Checa se a pilha está vazia
     if (vazia(pilha)) {
         printf("Pilha inicialmente vazia.\n");
     }
 
     printf("Empilhando sensores...\n");
 
+    // Cria os sensores MPU
     for (int i = 0; i < 3; i++) {
         MPU* sensor = criar_mpu(i, i);
         setar_aceleracao(sensor, 1000 * (i + 1), 2000 * (i + 1), 3000 * (i + 1));
@@ -18,6 +21,7 @@ int main() {
         printf("Sensor %d empilhado com sucesso.\n", i + 1);
     }
 
+    // Checagens para saber se pilha está cheia e se é possível adcionar o sensor a ela
     if (cheia(pilha)) {
         printf("A pilha está cheia! Não é possível empilhar mais sensores.\n");
     } else {
@@ -26,6 +30,7 @@ int main() {
         printf("Sensor extra empilhado com sucesso.\n");
     }
 
+    // Desempilha os sensores e exibe seus dados
     printf("\nDesempilhando sensores...\n");
     while (!vazia(pilha)) {
         MPU* sensor = desempilhar(pilha);
@@ -40,10 +45,12 @@ int main() {
         apagar_mpu(sensor);
     }
 
+    // Checa se a pilha está vazia após desempilhar os sensores
     if (vazia(pilha)) {
         printf("Pilha vazia após desempilhar todos os sensores.\n");
     }
 
+    // Apaga a pilha
     apagar_pilha(pilha);
 
     return 0;
