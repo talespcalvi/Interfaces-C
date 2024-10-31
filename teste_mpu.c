1a3f1f3fc8bf8e9aa0274eada07839ef93d95df2
Tales Paiva <124717378+talespcalvi@users.noreply.github.com>
2024-10-31 13:49:37 -0300

    Update teste_mpu.c

--- teste_mpu.c
+++ teste_mpu.c
@@ -3,22 +3,25 @@
 
 int main(void) 
 {
-  MPU* sensor = criar_mpu(0, 0);
+  MPU* sensor = criar_mpu(0, 0); // Cria o sensor MPU com sensibilidade de ±2g e ±250°/s
   float acX, acY, acZ, rotX, rotY, rotZ;
 
-  setar_aceleracao(sensor, 16384, 0, 0);
-  setar_rotacao(sensor, 131, 0, 0);
+  // Define os valores brutos
+  setar_aceleracao(sensor, 16384, 0, 0); // 1g no eixo x
+  setar_rotacao(sensor, 131, 0, 0);      // 1°/s no eixo X
 
+  // Indexa os valores ao sensor
   pegar_aceleracao(sensor, &acX, &acY, &acZ);
   pegar_rotacao(sensor, &rotX, &rotY, &rotZ);
 
+  // Calcula dos valores de aceleração e rotação
   printf("Teste 1:\n");
   printf("Aceleração: X = %.2fg, Y = %.2fg, Z = %.2fg\n", acX, acY, acZ);
   printf("Rotação: X = %.2f°/s, Y = %.2f°/s, Z = %.2f°/s\n", rotX, rotY, rotZ);
 
   
-  setar_aceleracao(sensor, 8192, -8192, 4096); 
-  setar_rotacao(sensor, 65, -65, 33);           
+  setar_aceleracao(sensor, 8192, -8192, 4096); // ±4g nos eixos
+  setar_rotacao(sensor, 65, -65, 33);          // ±500°/s nos eixos
 
   pegar_aceleracao(sensor, &acX, &acY, &acZ);
   pegar_rotacao(sensor, &rotX, &rotY, &rotZ);
@@ -28,8 +31,8 @@ int main(void)
   printf("Rotação: X = %.4f°/s, Y = %.4f°/s, Z = %.4f°/s\n\n", rotX, rotY, rotZ);
 
   
-  setar_aceleracao(sensor, -4096, -4096, -4096); 
-  setar_rotacao(sensor, -32, -32, -32);
+  setar_aceleracao(sensor, -4096, -4096, -4096); // -8g nos três eixos
+  setar_rotacao(sensor, -32, -32, -32);          // -1000°/s nos três eixos 
 
   pegar_aceleracao(sensor, &acX, &acY, &acZ);
   pegar_rotacao(sensor, &rotX, &rotY, &rotZ);
@@ -39,8 +42,8 @@ int main(void)
   printf("Rotação: X = %.4f°/s, Y = %.4f°/s, Z = %.4f°/s\n\n", rotX, rotY, rotZ);
 
  
-  setar_aceleracao(sensor, 1024, 512, 256);     
-  setar_rotacao(sensor, 16, 8, 4);               
+  setar_aceleracao(sensor, 1024, 512, 256); // ±16g com pequenos valores     
+  setar_rotacao(sensor, 16, 8, 4);          // ±2000°/s com pequenos valores               
 
   pegar_aceleracao(sensor, &acX, &acY, &acZ);
   pegar_rotacao(sensor, &rotX, &rotY, &rotZ);
@@ -50,8 +53,8 @@ int main(void)
   printf("Rotação: X = %.4f°/s, Y = %.4f°/s, Z = %.4f°/s\n\n", rotX, rotY, rotZ);
 
   
-  setar_aceleracao(sensor, 100, 50, 25); 
-  setar_rotacao(sensor, 5, 2, 1); 
+  setar_aceleracao(sensor, 100, 50, 25); // Valores menores que os tabelados
+  setar_rotacao(sensor, 5, 2, 1);        // Valores menores que os tabelados
 
   pegar_aceleracao(sensor, &acX, &acY, &acZ);
   pegar_rotacao(sensor, &rotX, &rotY, &rotZ);
